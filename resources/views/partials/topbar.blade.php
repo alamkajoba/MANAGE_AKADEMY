f082753deff6c5544845a4e68e915191df203e67
alamkajoba <alamkajoba007@gmail.com>
2025-07-09 16:21:32 +0200

    fix login

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -46,7 +46,8 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Kajoba Alam</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{auth()->user()->name}}</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{auth()->user()->email}}</span>
                                 <img class="img-profile rounded-circle"
                                     src="{{asset('img/undraw_profile.svg')}}">
                             </a>
